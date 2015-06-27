8142017238e29bd8b46125970438d17caa086346
Noe Nevarez <closner_nevarez@hotmail.com>
2015-06-27 17:12:44 -0500

    fixed sticky on navbar

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -1,77 +1,96 @@
-
-	<!-- navbar start -->
-	<!-- ================ -->
-	<nav class="navbar navbar-default" role="navigation">
-	<div class="container-fluid">
-
-	<!-- Toggle get grouped for better mobile display -->
-	<div class="navbar-header">
-	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
-	<span class="sr-only">Toggle navigation</span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-	</button>
-	</div>
-
-	<!-- Collect the nav links, forms, and other content for toggling -->
-	<div class="collapse navbar-collapse" id="navbar-collapse-1">
-	<ul class="nav navbar-nav navbar-right">
-	<li class="dropdown active">
-	<a href="{{{ action('HomeController@showHome')}}}">Home</a>
-	</li>
-	<li class="dropdown">
-	<a href="#about" class="page-scroll">Contact</a>
-														
-	<!-- mega-menu start -->
-	<li class="dropdown mega-menu">
-	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Services</a>
-	<ul class="dropdown-menu">
-	<li>
+<!-- header start classes:
+				fixed: fixed navigation mode (sticky menu) e.g. <header class="header fixed clearfix">
+				 dark: dark header version e.g. <header class="header dark clearfix">
+			================ -->
+<header class="header fixed clearfix">
+<div class="container">
 	<div class="row">
-	<div class="col-lg-4 col-md-3 hidden-sm">
-		<h4>Premium HTML5 Template</h4>
-		<p>iDea is perfectly suitable for corporate, business and company webpages.</p>
-		<img src="images/section-image-3.png" alt="iDea">
-	</div>
-	<div class="col-lg-8 col-md-9">
-		<h4>Pages</h4>
-		<div class="row">
-			<div class="col-md-4">
-				<div class="divider"></div>
-				<ul class="menu">
-					<li><a href="page-about.html"><i class="icon-right-open"></i>About Us</a></li>
-					<li><a href="page-about-2.html"><i class="icon-right-open"></i>About Us 2</a></li>
-					<li><a href="page-about-3.html"><i class="icon-right-open"></i>About Us 3</a></li>
-					<li><a href="page-about-me.html"><i class="icon-right-open"></i>About Me</a></li>
-					<li><a href="page-team.html"><i class="icon-right-open"></i>Our Team - Options</a></li>
-					<li><a href="page-services.html"><i class="icon-right-open"></i>Services</a></li>
-				</ul>
-			</div>
-			<div class="col-md-4">
-				<div class="divider"></div>
-				<ul class="menu">
-					<li><a href="page-contact.html"><i class="icon-right-open"></i>Contact</a></li>
-					<li><a href="page-contact-2.html"><i class="icon-right-open"></i>Contact 2</a></li>
-					<li><a href="page-coming-soon.html"><i class="icon-right-open"></i>Coming Soon Page</a></li>
-					<li><a href="page-404.html"><i class="icon-right-open"></i>404 error</a></li>
-					<li><a href="page-faq.html"><i class="icon-right-open"></i>FAQ page</a></li>
-					<li><a href="page-affix-sidebar.html"><i class="icon-right-open"></i>Sidebar - Affix Menu</a></li>
-				</ul>
-			</div>
-			<div class="col-md-4">
-				<div class="divider"></div>
-				<ul class="menu">
-					<li><a href="page-left-sidebar.html"><i class="icon-right-open"></i>Left Sidebar</a></li>
-					<li><a href="page-right-sidebar.html"><i class="icon-right-open"></i>Right Sidebar</a></li>
-					<li><a href="page-two-sidebars.html"><i class="icon-right-open"></i>Two Sidebars</a></li>
-					<li><a href="page-no-sidebar.html"><i class="icon-right-open"></i>No Sidebars</a></li>
-					<li><a href="page-sitemap.html"><i class="icon-right-open"></i>Sitemap</a></li>
-					<li><a href="page-invoice.html"><i class="icon-right-open"></i>Invoice <span class="badge">v1.1</span></a></li>
-				</ul>
-			</div>
-	</nav>
-	<!-- navbar end -->
+		<div class="col-md-3">
 
+			<!-- header-left start -->
+			<!-- ================ -->
+			<div class="header-left clearfix">
+
+				<!-- logo -->
+				<div class="logo">
+					<a href="index.html"><img id="logo" src="images/logo_red.png" alt="iDea"></a>
+				</div>
+
+				<!-- name-and-slogan -->
+				<div class="site-slogan">
+					Clean &amp; Powerful Bootstrap Theme
+				</div>
+
+			</div>
+			<!-- header-left end -->
+
+		</div>
+		<div class="col-md-9">
+
+			<!-- header-right start -->
+			<!-- ================ -->
+			<div class="header-right clearfix">
+
+				<!-- main-navigation start -->
+				<!-- ================ -->
+				<div class="main-navigation animated">
+
+					<!-- navbar start -->
+					<!-- ================ -->
+					<nav class="navbar navbar-default" role="navigation">
+						<div class="container-fluid">
+
+							<!-- Toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse" id="navbar-collapse-1">
+								<ul class="nav navbar-nav navbar-right">
+									<li class="dropdown">
+										<a href="{{{action('HomeController@showHome')}}}" class="dropdown-toggle" data-toggle="dropdown">Home</a>
+									</li>
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Services</a>
+										<ul class="dropdown-menu">
+											<li class="dropdown">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown">Residentional/Commercial</a>
+											</li>
+											<li class="dropdown">
+												<a href="#" class="dropdown-toggle" data-toggle="dropdown">Car Wash/Detailing</a>
+											</li>
+											<li class="dropdown">
+											</li>
+											<li class="dropdown">
+											</li>												
+										</ul>
+									</li>													
+									<!-- mega-menu start -->
+									<li class="dropdown active mega-menu">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Contact Us</a>
+									</li>
+									<!-- mega-menu end -->
+								</ul>
+							</div>
+
+						</div>
+					</nav>
+					<!-- navbar end -->
+
+				</div>
+				<!-- main-navigation end -->
+
+			</div>
+			<!-- header-right end -->
+
+		</div>
 	</div>
-	<!-- main-navigation end -->
+</div>
+</header>
+			<!-- header end -->
