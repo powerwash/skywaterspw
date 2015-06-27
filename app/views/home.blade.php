f3484a18e5e7749a6a718f628302f328aa8ad376
Noe Nevarez <closner_nevarez@hotmail.com>
2015-06-27 15:16:15 -0500

    created landing page

---
+++ app/views/home.blade.php
@@ -0,0 +1,329 @@
+@extends('layouts.master')
+<head>
+@section('title')Home
+@stop
+</head>
+@section('dashboard')
+	<body class="front no-trans">
+		<!-- scrollToTop -->
+		<!-- ================ -->
+		<div class="scrollToTop"><i class="icon-up-open-big"></i></div>
+
+		<!-- page wrapper start -->
+		<!-- ================ -->
+		<div class="page-wrapper">
+
+			<!-- header start classes:
+				fixed: fixed navigation mode (sticky menu) e.g. <header class="header fixed clearfix">
+				 dark: dark header version e.g. <header class="header dark clearfix">
+			================ -->
+			<header class="header fixed header-small clearfix">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3">
+
+							<!-- header-left start -->
+							<!-- ================ -->
+							<div class="header-left clearfix">
+
+								<!-- logo -->
+								<div class="logo">
+									<a href="index.html"><img id="logo" src="images/logo_red.png" alt="iDea"></a>
+								</div>
+
+								<!-- name-and-slogan -->
+								<div class="site-slogan">
+									Clean &amp; Powerful Bootstrap Theme
+								</div>
+
+							</div>
+							<!-- header-left end -->
+
+						</div>
+						<div class="col-md-9">
+
+							<!-- header-right start -->
+							<!-- ================ -->
+							<div class="header-right clearfix">
+
+								<!-- main-navigation start -->
+								<!-- ================ -->
+								<div class="main-navigation animated">
+
+									
+							</div>
+							<!-- header-right end -->
+
+						</div>
+					</div>
+				</div>
+			</header>
+			<!-- header end -->
+
+			
+
+			<!-- page-top start-->
+			<!-- ================ -->
+			<div class="page-top object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="100">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<h1 class="text-center title">Services</h1>
+							<div class="separator"></div>
+							<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+							<div class="row grid-space-20">
+								<div class="col-sm-12 col-md-4 col-md-push-4">
+									<img src="images/section-image-5.png" alt="" class="object-non-visible" data-animation-effect="fadeInUp" data-effect-delay="0">
+								</div>
+								<div class="col-sm-6 col-md-4 col-md-pull-4">
+									<div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="0">
+										<div class="icon-container default-bg">
+											<i class="fa fa-bold"></i>
+										</div>
+										<div class="body">
+											<h2>Bootstrap Based</h2>
+											<p>Voluptatem ad provident non veritatis.</p>
+											<a href="page-services.html" class="link"><span>Read More</span></a>
+										</div>
+									</div>
+									<div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
+										<div class="icon-container default-bg">
+											<i class="fa fa-check"></i>
+										</div>
+										<div class="body">
+											<h2>Unlimited Options</h2>
+											<p>Iure sequi unde hic. Sapiente quaerat.</p>
+											<a href="page-services.html" class="link"><span>Read More</span></a>
+										</div>
+									</div>
+									<div class="box-style-3 right object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="400">
+										<div class="icon-container default-bg">
+											<i class="fa fa-bicycle"></i>
+										</div>
+										<div class="body">
+											<h2>Easy to Use</h2>
+											<p>Inventore dolores aut laboriosam cum.</p>
+											<a href="page-services.html" class="link"><span>Read More</span></a>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-6 col-md-4">
+									<div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="0">
+										<div class="icon-container default-bg">
+											<i class="fa fa-leaf"></i>
+										</div>
+										<div class="body">
+											<h2>Clean Design and Code</h2>
+											<p>Voluptatem ad provident non veritatis.</p>
+											<a href="page-services.html" class="link"><span>Read More</span></a>
+										</div>
+									</div>
+									<div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
+										<div class="icon-container default-bg">
+											<i class="fa fa-laptop"></i>
+										</div>
+										<div class="body">
+											<h2>Very Flexible</h2>
+											<p>Iure sequi unde hic. Sapiente quaerat.</p>
+											<a href="page-services.html" class="link"><span>Read More</span></a>
+										</div>
+									</div>
+									<div class="box-style-3 object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="400">
+										<div class="icon-container default-bg">
+											<i class="fa fa-sitemap"></i>
+										</div>
+										<div class="body">
+											<h2>Latest Technologies</h2>
+											<p>Inventore dolores aut laboriosam cum.</p>
+											<a href="page-services.html" class="link"><span>Read More</span></a>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- page-top end -->
+
+			<!-- main-container start -->
+			<!-- ================ -->
+			<section class="main-container default-bg">
+
+				<!-- main start -->
+				<!-- ================ -->
+				<div class="main">
+					<div class="container">
+						<div class="call-to-action">
+							<div class="row">
+								<div class="col-md-8">
+									<h1 class="title text-center">We offer more possibilities to meet your every need.</h1>
+								</div>
+								<div class="col-md-4">
+									<div class="text-center">
+										<a href="#" class="btn btn-white btn-lg">Purchase</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- main end -->
+
+			</section>
+			<!-- main-container end -->
+
+			<!-- section start -->
+			<!-- ================ -->
+			<div class="section clearfix object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="200">
+				<div class="container">
+					<h1 class="text-center">Who We Are</h1>
+					<div class="separator"></div>
+					<p class="lead text-center">Lorem ipsum dolor sit amet. Maiores, odio ut deleniti doloremque ad at praesentium<br> laudantium molestias similique. Quisquam incidunt ut laboriosam.</p>
+					<br>
+					<div class="row">
+						<div class="col-md-6">
+							<img src="images/section-image-4.png" alt="">
+							<div class="space hidden-md hidden-lg"></div>
+						</div>
+						<div class="col-md-6">
+							<p class="space-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi veniam, veritatis sint consequuntur! Natus, culpa, explicabo! Non quo laboriosam nobis quia, amet excepturi optio dolor impedit. Perferendis libero unde, nulla deserunt sequi. Voluptatum, officia suscipit porro reprehenderit vitae quo rem reiciendis, a vero quae. Vero beatae voluptatum, illo quaerat provident doloribus consequuntur saepe, id quas animi vel ad eligendi adipisci.</p>
+							<a href="page-about-2.html" class="btn btn-lg btn-default">Read More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- section end -->
+
+			<!-- section start -->
+			<!-- ================ -->
+			<div class="section gray-bg clearfix object-non-visible" data-animation-effect="fadeInUpSmall" data-effect-delay="300">
+				<div class="container">
+
+					<h2 class="title">Our Team</h2>
+					<div class="separator-2"></div>
+
+					<div class="row grid-space-20">
+						<div class="col-md-3 col-sm-6">
+							<div class="box-style-1 white-bg team-member">
+								<div class="overlay-container">
+									<img src="images/team-member-1.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<h3>Jane Doe</h3>
+								CEO
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="box-style-1 white-bg team-member">
+								<div class="overlay-container">
+									<img src="images/team-member-2.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<h3>John Doe</h3>
+								CTO
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="box-style-1 white-bg team-member">
+								<div class="overlay-container">
+									<img src="images/team-member-3.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<h3>Anne Smith</h3>
+								Project Manager
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<div class="box-style-1 white-bg team-member">
+								<div class="overlay-container">
+									<img src="images/team-member-4.jpg" alt="">
+									<div class="overlay">
+										<ul class="social-links clearfix">
+											<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+											<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+											<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								<h3>Margaret Manson</h3>
+								Developer
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- section end -->
+
+			
+
+			
+
+			<!-- section start -->
+			<!-- ================ -->
+			<div class="section gray-bg text-muted footer-top clearfix">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="owl-carousel clients">
+								<div class="client">
+									<a href="#"><img src="/images/client-1.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="/images/client-2.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="/images/client-3.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="i/mages/client-4.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="/images/client-5.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="/images/client-6.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="/images/client-7.png" alt=""></a>
+								</div>
+								<div class="client">
+									<a href="#"><img src="/images/client-8.png" alt=""></a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<blockquote class="inline">
+								<p class="margin-clear">Design is not just what it looks like and feels like. Design is how it works.</p>	
+								<footer><cite title="Source Title">Steve Jobs </cite></footer>
+							</blockquote>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- section end -->
+
+			
+
+		</div>
+		<!-- page-wrapper end -->
+@stop
